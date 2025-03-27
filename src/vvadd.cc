d0839546373da2cc7a85430a91eca56fd42c5ef9
nfc35 <nfc35@cornell.edu>
2025-03-27 14:04:49 -0400

    add vvadd program

---
+++ src/vvadd.cc
@@ -0,0 +1,26 @@
+#include <array>
+#include <gem5/m5ops.h>
+
+const int arr_size = 10000;
+const int reps = 10;
+
+int main()
+{
+    std::array<int, arr_size> a;
+    std::array<int, arr_size> b;
+    std::array<int, arr_size> c;
+
+    for ( int i = 0; i < arr_size; i++ ) {
+        a[i] = i;
+        b[i] = i * i;
+        c[i] = 0;
+    }
+
+    m5_dump_reset_stats( 0, 0 );
+    for ( int j = 0; j < reps; j++ ) {
+        for ( int i = 0; i < arr_size; i++ ) {
+            c[i] = a[i] + b[i];
+        }
+    }
+    m5_dump_reset_stats( 0, 0 );
+}
